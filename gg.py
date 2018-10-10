8edf1acc48d93277e0c8339170eabf4e3bd34613
Lele Chen <lchen63@j17.cs.rochester.edu>
2018-10-10 17:09:33 -0400

    g

--- gg.py
+++ gg.py
@@ -30,9 +30,9 @@ def get_fake():
 				psd = gg[0]
 				usrname = gg[1]
 				if fake_password == psd:
-					
+					ggg.write(line + ',' + psd + ',' + usrname + '\n')
 					result.append(line + ',' + psd + ',' + usrname)
-					ggg.write(result)
+					
 			count += 1
 			line = _file.readline()
 		except:
