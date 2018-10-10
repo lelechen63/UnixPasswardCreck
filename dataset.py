7d6dba89922a245e5a1f49c1d8f113defe921590
Lele Chen <lchen63@j17.cs.rochester.edu>
2018-10-10 13:55:26 -0400

    initial commit

--- dataset.py
+++ dataset.py
@@ -1,35 +1,22 @@
+import torch.utils.data as data
+from crypt import crypt
+import numpy
+import torch
 class Password(data.Dataset):
-    def __init__(self,
-                 dataset_dir,
-                 output_shape=[128, 128],
-                 train='train'):
-        self.train = train
-        self.root ='/mnt/disk1/dat/lchen63/vgg_face_dataset/'
-        _file = open(os.path.join(dataset_dir, "test.pkl"), "rb")
-        self.train_data = pickle.load(_file)
-        _file.close()
+    def __init__(self):
+
+        self._file = open( "./pool.txt", "rb")
+        self.train_data = _file.readlines()
+        self._file.close()
        
     def __getitem__(self, index):
-        # In training phase, it return real_image, wrong_image, text
-                # try:
-        print ('------')
-        image_path =  self.train_data[index][0]
-        label = self.train_data[index][1]
-        # print ('------')
-
-        im = cv2.imread(image_path)
-        if im is None:
-            raise IOError
-        im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
-        im = cv2.resize(im, self.output_shape)
-        im = self.transform(im)
-        # print ('------')
-
-
-        return im, label
-
-           
-
+        line = self.train_data[index][:-1]
+        fake_password = crypt(line, '00')[2:]
+        for ch in line:
+            num.append(ord(ch)) 
+        num = np.asarray(num)
+        num = torch.Tensor(num)
+        return num 
     def __len__(self):
         return len(self.train_data)
        
--- gg.py
+++ gg.py
@@ -1,3 +1,65 @@
 from crypt import crypt
-import pytorch
-import 
+# import torch
+from dataset import Password
+import argparse
+import os
+import glob
+import time
+import torch
+from torch.utils.data import DataLoader
+import numpy as np
+from collections import OrderedDict
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--batch_size",
+                        type=int,
+                        default=1)
+    
+    parser.add_argument("--cuda",
+                        default=True)  
+    parser.add_argument('--device_ids', type=str, default='3')
+    parser.add_argument('--num_thread', type=int, default=1)
+    # parser.add_argument('--flownet_pth', type=str, help='path of flownets model')
+
+    return parser.parse_args()
+config = parse_args()
+
+def get_real():
+	_file = open( "./password.txt", "rb")
+	train_data = _file.readlines()
+	_file.close()
+	real = []
+
+	for line in train_data:
+		num = []
+		line = line[:-1]
+		fake_password = crypt(line, '00')[2:]
+		print (fake_password)
+		for ch in line:
+			num.append(ord(ch)) 
+		print(num)
+		real.append(num)
+
+	real = np.asarray(real)
+	real = torch.Tensor(real)
+	return real
+
+def test():
+	os.environ["CUDA_VISIBLE_DEVICES"] = config.device_ids
+	dataset =  Password()
+	data_loader = DataLoader(dataset,
+	                                  batch_size=config.batch_size,
+	                                  num_workers= config.num_thread,
+	                                  shuffle=False, drop_last=True)
+	
+	for step, (gt, fake_password) in enumerate(data_loader):
+		if config.cuda:
+			fake_password = fake_password.cuda()
+			real_password = get_real().cuda()
+		diff = fake_password - real_password
+		diff = torch.sum(diff, dim= 1)
+		print ((diff == 0).nonzero())
+
+test()
