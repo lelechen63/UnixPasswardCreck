a6360f72b5bfb8dc9bf6bf3a52f7a766e1142984
Lele Chen <lchen63@j17.cs.rochester.edu>
2018-10-10 17:02:17 -0400

    g

--- gg.py
+++ gg.py
@@ -7,52 +7,57 @@ def get_real():
 	_file.close()
 	real = []
 	for line in train_data:
-		num = {}
+		num = []
 		usrname, psd  = line.split()
 		psd = psd[2:]
-		num[psd] = usrname
+		num.append(psd)
+		num.append(usrname)
 		real.append(num)
 	return real
-def get_fake(index):
+def get_fake():
+	ggg = open( "./ggg.txt", "w")
 	_file = open( "./crackstation.txt", "rb")
 	line = _file.readline()
-	big_pool = {}
 	count = 0
-	while line:
-		if count < index * 1000000:
-			continue
-		try:
-			line = line[:-1]
-			fake_password = crypt(line, '00')[2:]
-			big_pool[fake_password] = line
-			count += 1
-			line = f.readline()
-		except:
-			line = f.readline()
-			continue
-		
-		if count == 1000000 * (index + 1):
-			return big_pool
-	return big_pool    
-
-def multi_pool(index):
 	real =get_real()
-	fake = get_fake(index)
-	num_thread = 4
-	for i in range(num_thread):
-	    process = multiprocessing.Process(
-	        target=find_same, args=(real[i], fake,))
-	    process.start()
+	while line:
+		print (count)
+		# try:
+		line = line[:-1]
+		fake_password = crypt(line, '00')[2:]
+		for gg in real:
+			psd = gg[0]
+			usrname = gg[1]
+			if fake_password == psd:
+				ggg.write(line + ',' + psd + ',' + usrname + '/n')
+			else:
+				print (line + ',' + psd + ',' + usrname + '/n')
+				print (fake_password)
+				print ('=====')
+		count += 1
+		line = f.readline()
+		# except:
+		# 	line = f.readline()
+		# 	continue
+get_fake()	
+# def multi_pool(index):
+# 	real =get_real()
+# 	fake = get_fake(index)
+# 	num_thread = 4
+# 	for i in range(num_thread):
+# 	    process = multiprocessing.Process(
+# 	        target=find_same, args=(real[i], fake,))
+# 	    process.start()
 
 
-def find_same(reals, fake):
-	psd = reals.keys()[0]
+# def find_same(reals, fake):
+# 	psd = reals.keys()[0]
 	
-	for index, key in enumerate(fake) :
-		if key == psd:
-			print ('==============')
-			print (fake[key], key)
-for i in range(1300):
-	multi_pool(i)
+# 	for index, key in enumerate(fake) :
+# 		if key == psd:
+# 			print ('==============')
+# 			print (fake[key], key)
 
-multi_pool()
+
+# for i in range(1300):
+# 	multi_pool(i)
